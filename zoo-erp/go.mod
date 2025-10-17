3ee86004dd70fe106c1eaac81cc2f8664fd5bbc5
Zuev Akim <beliashkoff@vk.com>
2025-10-17 20:53:55 +0300

    feat: create container

--- zoo-erp/go.mod
+++ zoo-erp/go.mod
@@ -1,3 +1,5 @@
 module github.com/Beliashkoff/HSE-SE-software-design-HW1/zoo-erp
 
 go 1.24.4
+
+require go.uber.org/dig v1.17.1
---
+++ zoo-erp/internal/app/container.go
@@ -0,0 +1,24 @@
+package app
+
+import (
+	"zoo-erp/internal/repo"
+	"zoo-erp/internal/services"
+
+	"go.uber.org/dig"
+)
+
+// Build — composition root: регистрируем все зависимости.
+func Build() *dig.Container {
+	c := dig.New()
+
+	// Репозитории (in-memory)
+	_ = c.Provide(repo.NewAnimalMem)
+	_ = c.Provide(repo.NewThingMem)
+
+	// Сервисы и зависимости
+	_ = c.Provide(services.NewSimpleVet)
+	_ = c.Provide(services.NewSeq)
+	_ = c.Provide(services.NewZoo)
+
+	return c
+}
